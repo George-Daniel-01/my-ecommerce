28bebb776b32b7f29f9337c919d80e67af161903
Gerorg Daniel <georgeabiamakdanie>
2025-08-06 19:09:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Narbar from './components/Narbar/Narbar'
+
+export const App = () => {
+  return (
+    <div className='app'>
+       <Narbar/>
+    </div>
+  )
+}
